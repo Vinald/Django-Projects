368e3a9a0da11bdcc34e6a28c144ba8262e4c99c
[vinald] <okiror1vinald@gmail.com>
2025-10-26 20:09:50 +0300

    models

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class Airport(models.Model):
+    code = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} - {self.name} ({self.city}, {self.country})"
+
 class Flight(models.Model):
     flight_number = models.CharField(max_length=10)
-    departure = models.CharField(max_length=100)
-    arrival = models.CharField(max_length=100)
+    departure = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
+    arrival = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     duration = models.IntegerField()
